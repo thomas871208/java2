12d395c67eff6043262cf7a7bf60bfea32755860
Thomas <Thomas@Thomas_pc>
2019-06-17 18:57:22 +0800

    no good

---
+++ Collection/src/com/TTS/Cities.java
@@ -0,0 +1,11 @@
+package com.TTS;
+
+public class Cities {
+
+		String city;
+	public void Cities(String city) {
+		this.city = city;
+		System.out.println(city);
+	}
+
+}
--- Collection/src/com/TTS/Taiwan.java
+++ Collection/src/com/TTS/Taiwan.java
@@ -2,19 +2,34 @@ package com.TTS;
 
 import java.io.*;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Taiwan {
 
 	public static void main(String[] args) {
+
+		ArrayList<String>cities = new ArrayList<>();
+		ArrayList<String>areas = new ArrayList<>();
+		ArrayList<String>postcode = new ArrayList<>();
+		String lastcity ="";
 		try{
 	BufferedReader in = new BufferedReader(new FileReader("Data.txt"));
 	String line = in.readLine();
+
 	while(line!=null){
-		System.out.println(line);
-//		String[] tokens =line.split(",");
-//		System.out.println(tokens[0] + tokens[1]+tokens[2]);
+//		System.out.println(line);
+		String[] tokens =line.split(",");
+		if(!tokens[0].equals(lastcity)) {
+			lastcity = tokens[0];
+			cities.add(lastcity);
+		}
+		areas.add(tokens[1]);
+		postcode.add(tokens[2]);
+		line = in.readLine();
 	}
-	}catch(IOException e) {
+	System.out.println( cities);
+
+	}catch(Exception e) {
 		e.getStackTrace();
 	}
 		
